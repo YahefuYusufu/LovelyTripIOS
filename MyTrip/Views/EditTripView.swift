9942ca9f9f5c7062478292a7cebcfcc96bf40dd3
YahefuYusufu <parguzatt@gmail.com>
2024-01-10 22:15:16 +0100

    edit view in progress.

---
+++ MyTrip/Views/EditTripView.swift
@@ -0,0 +1,18 @@
+//
+//  EditTripView.swift
+//  MyTrip
+//
+//  Created by yusufyakuf on 2024-01-10.
+//
+
+import SwiftUI
+
+struct EditTripView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditTripView()
+}
