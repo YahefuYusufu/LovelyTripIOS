8c673ada2523e627b8e9a28e50585a01d79b0c8e
YahefuYusufu <parguzatt@gmail.com>
2024-01-13 14:58:12 +0100

    check the repo is working

---
+++ MyTrip/Views/EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  MyTrip
+//
+//  Created by yusufyakuf on 2024-01-13.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
